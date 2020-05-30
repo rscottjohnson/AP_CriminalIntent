cf97be506db43913a6e06d057636449d9c3784b3
rscottjohnson <sjohnson.wvu@gmail.com>
2020-05-30 17:27:02 -0400

    Added row and button for more serious crimes

--- app/src/main/java/com/bignerdranch/android/ap_criminalintent/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/ap_criminalintent/Crime.kt
@@ -3,6 +3,7 @@ package com.bignerdranch.android.ap_criminalintent
 import java.util.*
 
 data class Crime(val id: UUID = UUID.randomUUID(),
-var title: String = "",
-var date: Date = Date(),
-var isSolved: Boolean = false)
+                 var title: String = "",
+                 var date: Date = Date(),
+                 var isSolved: Boolean = false,
+                 var requiresPolice: Boolean = false)
